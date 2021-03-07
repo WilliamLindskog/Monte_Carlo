8a3605091963d0e0371dcb6ffdfa4bd182a52412
WilliamLindskog <williamlindskog@outlook.com>
2021-03-07 11:12:12 +0100

    Question 2 done

--- Lab 3/generate_parameters.m
+++ Lab 3/generate_parameters.m
@@ -1,7 +1,5 @@
 % GENERATE NECESSARY PARAMETERS
-function [alpha, alpha2, beta_array, mu_array, delta] = generate_parameters(samples, significance_lvl)
-    alpha = (significance_lvl/2)*samples;
-    alpha2  =(1-(significance_lvl/2))*samples;
+function [beta_array, mu_array, delta] = generate_parameters(samples, significance_lvl)
     beta_array = zeros(samples, 1);
     mu_array = zeros(samples, 1);
     delta = zeros(samples, 2); 
